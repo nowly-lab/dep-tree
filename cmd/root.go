404d5e02ffa24c9269db667a5b86743ed5be0940
yk-matsu <yuki.matsuyama0123@gmail.com>
2025-12-07 00:55:16 +0900

    chore: bump version to v0.23.16

--- cmd/root.go
+++ cmd/root.go
@@ -33,7 +33,7 @@ func NewRoot(args []string) *cobra.Command {
 
 	root := &cobra.Command{
 		Use:               "nowly-tree",
-		Version:           "v0.23.15",
+		Version:           "v0.23.16",
 		Short:             "Visualize and check your project's dependency graph",
 		SilenceUsage:      true,
 		Args:              cobra.ArbitraryArgs,
